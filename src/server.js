c05f2b1fec450da4b91f5dffe5834c7e8ab2c7fd
phannhuquyen <phannhuquyen88@gmail.com>
2026-05-10 11:07:36 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -16,7 +16,13 @@ const app = express();
 //
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  }),
+);
 
 // app.use(express.json());
 app.use("/api/users", userRoute);
