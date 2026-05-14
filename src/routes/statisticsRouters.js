8fe4ffbe0c44d3734e93dfad2ad5098640889c2e
phannhuquyen <phannhuquyen88@gmail.com>
2026-05-14 14:27:18 +0700

    thêm api thống kê theo ngày

--- src/routes/statisticsRouters.js
+++ src/routes/statisticsRouters.js
@@ -1,8 +1,12 @@
+// biểu đồ thống kê
+import express from "express";
 import {
   getRevenueStatistics,
   getRevenueStatisticsByDay,
 } from "../controllers/statisticsControllers.js";
 
+const router = express.Router();
 router.get("/revenue", getRevenueStatistics);
-
 router.get("/revenue/day", getRevenueStatisticsByDay);
+
+export default router;
